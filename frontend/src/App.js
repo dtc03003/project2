fea792fb360f4c7e8ac3935b5e3c9be67e2f1eb0
박현진 <jinnydiary@naver.com>
2022-04-01 17:09:25 +0900

    [BE-박현진][faceai-106] feat: 얼굴 인식+로딩페이지 구현(FE, 버튼 제외)
    Former-commit-id: 6fbada5b05fc5806dec1a1936acbd6ae9cc4897e

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,6 +9,7 @@ import LoginPage from "./components/views/LoginPage/LoginPage";
 import SignupPage from "./components/views/SignupPage/SignupPage";
 import TransferPage from "./components/views/TransferPage/TransferPage";
 import FaceAIPage from "./components/views/FaceAIPage/FaceAIPage";
+import FaceLoadingPage from "./components/views/FaceAIPage/FaceLoadingPage";
 
 function App() {
   return (
@@ -21,6 +22,7 @@ function App() {
           <Route exact path="/signup" element={<SignupPage />} />
           <Route exact path="/transfer" element={<TransferPage />} />
           <Route exact path="/faceai" element={<FaceAIPage />} />
+          <Route exact path="/faceloading" element={<FaceLoadingPage />} />
           <Route path={"*"} element={<NotFound />} />
         </Routes>
       </BrowserRouter>
--- frontend/src/components/views/FaceAIPage/FaceAIPage.jsx
+++ frontend/src/components/views/FaceAIPage/FaceAIPage.jsx
@@ -1,34 +1,42 @@
-import React from 'react'
+import React from 'react';
 import ReactDOM from 'react-dom';
 import Webcam from "react-webcam";
 import { faceAI } from '../../../_actions/naverClovar';
+// import {CameraTwoTone} from '@ant-design/icons';
+import "./FaceAI.css";
 
 export default function FaceAI() {
-        const WebcamCapture = () => {
-        const webcamRef = React.useRef(null);
-        const [imgSrc, setImgSrc] = React.useState(null);
-        
-        const capture = React.useCallback(() => {
+    const WebcamCapture = () => {
+    const webcamRef = React.useRef(null);
+
+    const capture = React.useCallback(() => {
         const imageSrc = webcamRef.current.getScreenshot();
-        setImgSrc(imageSrc);
         faceAI(imageSrc);
-    }, [webcamRef, setImgSrc]);
+    }, [webcamRef]);
 
     return (
-        <>
-            <h1>얼굴 인식 API 구현중</h1>
-            <Webcam
-            audio={false}
-            ref={webcamRef}
-            screenshotFormat="image/jpeg"
-            />
-            <button onClick={capture}>사진 찍기</button>
-            {imgSrc && (
-                <img src={imgSrc} alt="img for Face AI"/>
-            )}
-        </>
-    );
-};
+        <div id="faceAIPage">
+            <div id="title">
+                <p id="titleName">예적금 추천 서비스</p>
+                <p id="faceAIInfo">카메라를 보고 사진기를 눌러주세요!</p>
+            </div>
+
+            <div id="webcam">
+                <Webcam
+                audio={false}
+                ref={webcamRef}
+                screenshotFormat="image/jpeg"
+                />
+            </div>
+            
+            <div id="webcamBtn">
+                {/* twoToneColor="" <- 색 지정 */}
+                <button onClick={capture}>임시버튼</button>
+                {/* <CameraTwoTone onClick={capture} style={{ fontSize: '5em'}}/> */}
+            </div>
+        </div>
+        );
+    };
 
     ReactDOM.render(<WebcamCapture />, document.getElementById("root"));
 }
