0b396e403f78f5d0a1a728a8740225c4ac633669
elenaisnanocat <elenassafy0201@gmail.com>
2022-04-04 00:26:56 +0900

    [FE-박유정][deposit-105] feat: 예적금 페이지 아이/어른/노령층 이미지
    Former-commit-id: 9a56a1372b60e270b0ee6ac673694b66607c1d95

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter, Route, Link, Routes } from "react-router-dom";
-import {ErrorBoundary} from 'react-error-boundary';
+import { ErrorBoundary } from "react-error-boundary";
 import LandingPage from "./components/views/LandingPage/LandingPage";
 import MainPage from "./components/views/MainPage/MainPage";
 import NotFound from "./components/views/NotFound/NotFound";
@@ -10,10 +10,11 @@ import SignupPage from "./components/views/SignupPage/SignupPage";
 import TransferPage from "./components/views/TransferPage/TransferPage";
 
 import DepositPage from "./components/views/DepositPage/DepositPage";
-import DepositCarousel from "./components/views/DepositPage/DepositCarousel";
+import DepositKidPage from "./components/views/DepositAgesPage/DepositKidPage";
+import DepositAdultPage from "./components/views/DepositAgesPage/DepositAdultPage";
+import DepositElderPage from "./components/views/DepositAgesPage/DepositElderPage";
 
 import FaceAIPage from "./components/views/FaceAIPage/FaceAIPage";
-
 
 function App() {
   return (
@@ -25,13 +26,12 @@ function App() {
           <Route exact path="/login" element={<LoginPage />} />
           <Route exact path="/signup" element={<SignupPage />} />
           <Route exact path="/transfer" element={<TransferPage />} />
-
           <Route exact path="/deposit" element={<DepositPage />} />
-          <Route exact path="/depositcarousel" element={<DepositCarousel />} />
-          
+          <Route exact path="/depositkid" element={<DepositKidPage />} />
+          <Route exact path="/depositadult" element={<DepositAdultPage />} />
+          <Route exact path="/depositelder" element={<DepositElderPage />} />
 
           <Route exact path="/faceai" element={<FaceAIPage />} />
-
           <Route path={"*"} element={<NotFound />} />
         </Routes>
       </BrowserRouter>
