797902d2cce70564017f3ba166b9dc3a20f2aca1
박현진 <jinnydiary@naver.com>
2022-04-06 22:11:45 +0900

    [BE-박현진][design-118] feat: 이벤트 페이지 추가(수정중)
    Former-commit-id: e84a71b3afbd4a047ba57f7cef961c28cc38feff

--- frontend/src/_actions/naverClovar.js
+++ frontend/src/_actions/naverClovar.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import Swal from 'sweetalert2'
 
 const BASE_URL = "https://j6d201.p.ssafy.io:9000/api/ai";
 // const BASE_URL = "http://localhost:9000/api/ai";
@@ -14,6 +15,34 @@ export function faceAI(image) {
         (res) => {
             sessionStorage.setItem("faceAge", res.data);
             window.location.href ='/faceloading';
+        }
+    )
+    return request;
+}
+
+export function eventfaceAI(image) {
+    const headers = {
+        "Access-Control-Allow-Origin" : "*"
+    }
+    const img = {
+        image : image
+    }
+    const request = axios.post(`${BASE_URL}/recognize/face`, img, {headers: headers}).then(
+        (res) => {
+            sessionStorage.setItem("eventFaceAge", res.data);
+            Swal.fire({
+                customClass: {
+                    container: 'swal-container'
+                },
+                icon: 'info',
+                html: `${sessionStorage.getItem("eventFaceAge")}`,
+                toast: true,
+                position: 'bottom-right',
+                confirmButtonText: '네',
+                })
+            .then(() => {
+                window.location.href='/main' //순위 페이지 만들어야디
+            })
         }
     )
     return request;
