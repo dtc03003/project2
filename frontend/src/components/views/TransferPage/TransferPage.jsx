d033b7621d3039964fc0e3af02c7afbfa3c0a769
김동현 <dtc03003@naver.com>
2022-04-06 18:09:52 +0900

    [FE-김동현][transfer-111] feat:이체페이지 구현완료
    Former-commit-id: 4e34bdc41ae05b920c251d9330708860132f6398

--- frontend/src/components/views/TransferPage/TransferPage.jsx
+++ frontend/src/components/views/TransferPage/TransferPage.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Header from '../MainPage/Header.jsx'
 import M_Body from './T_Body.jsx'
 import Footer from '../MainPage/Footer.jsx'
+import styles from './TransferPage.css'
 
 export default function Transferpage(){
     return (
