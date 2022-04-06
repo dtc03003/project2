8e1031d84ab27d3a54e057547031d7b3b76bcc83
김동현 <dtc03003@naver.com>
2022-04-06 10:17:50 +0000

    Update setupProxy.js
    Former-commit-id: c6419f192f83fded4aa6f59d5f1ca52101251227

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -3,7 +3,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 module.exports = function(app) {
   app.use(
     createProxyMiddleware('/api', {
-      target: 'https://j6d201.p.ssafy.io:9000', 
+      target: 'http://j6d201.p.ssafy.io:9000', 
       // 프론트엔드에서 벡엔드로 줄때 타겟을 설정하고 9000번으로 주겠다.
       changeOrigin: true,
     }),
