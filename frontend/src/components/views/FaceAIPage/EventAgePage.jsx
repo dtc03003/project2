d2c4e9681b3420b52db4b320b375dbde964754bc
박현진 <jinnydiary@naver.com>
2022-04-07 22:16:23 +0900

    [BE-박현진][design-118] perf: 얼굴 나이 맞추기 서비스 구현
    Former-commit-id: ab0e870f153dc0ac3a1f47f3fc47bdd3bd22968d

--- frontend/src/components/views/FaceAIPage/EventAgePage.jsx
+++ frontend/src/components/views/FaceAIPage/EventAgePage.jsx
@@ -1,13 +1,11 @@
 import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
 import Webcam from "react-webcam";
-// import { eventfaceAI } from '../../../_actions/naverClovar';
 import Camera from "../../../assets/camera.png"
 import "./FaceAI.css";
 
 import axios from "axios";
 import Swal from 'sweetalert2'
-import Record from './Record';
 
 const BASE_URL = "https://j6d201.p.ssafy.io:9000/api";
 
@@ -18,7 +16,6 @@ export default function EventAgePage() {
 
     const capture = React.useCallback(() => {
         const imageSrc = webcamRef.current.getScreenshot();
-        // eventfaceAI(imageSrc);
         const headers = {
             "Access-Control-Allow-Origin" : "*"
         }
@@ -55,7 +52,7 @@ export default function EventAgePage() {
         }
         axios.post(`${BASE_URL}/event/record`, participant, {headers: headers})
             .then((result) => {
-                console.log(result);
+                setInfo(result.data);
             })
 
     }, [webcamRef]);
@@ -64,15 +61,16 @@ export default function EventAgePage() {
         <div id="faceAIPage">
             <div id="title">
                 <h1 id="titleName">😆내 얼굴 나이는 몇 살일까?🤔</h1>
-                <h3 className="faceAIInfo">카메라를 보고 사진기를 눌러주세요!</h3>
+                <h4 className="faceAIInfo">카메라를 보고 사진기를 눌러주세요!</h4>
                 <h5 className="faceAIInfo">재미로 보는 것이니 재미로 참여해주세요~🤪</h5>
                 <input name="nickname" id="writeNick"
                     placeholder="결과를 기록하고 싶다면 닉네임을 입력해주세요" >
                 </input>
             </div>
 
-            <div id="webcam">
+            <div id="eventWebcam">
                 <Webcam
+                style={{height:'60vh'}}
                 audio={false}
                 ref={webcamRef}
                 screenshotFormat="image/jpeg"
@@ -85,65 +83,30 @@ export default function EventAgePage() {
 
             <div className='eventTable' id="eventShow">
                 <h6 className="eventFont">재미로 보는 얼굴 나이 기록들</h6>
-                <div className='eventTable'>
-                    <div className="eventFont thread">순번</div>
-                    <div className="eventFont thread">닉네임</div>
-                    <div className="eventFont thread">얼굴나이</div>
-                </div>
-            {/* <Record info={info} /> */}
+                <table id="evtable" className='eventTable'>
+                    <thead>
+                        <tr>
+                            <td>순번</td>
+                            <td>닉네임</td>
+                            <td>얼굴 나이</td>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {info.map((item) => (
+                            <tr key={item.no}>
+                                <td>{item.no}</td>
+                                <td>{item.nickname}</td>
+                                <td>{item.age}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
             </div>
+
         </div>
         );
     };
 
     ReactDOM.render(<WebcamCapture />, document.getElementById("root"));
-}
-
-export function eventfaceAI(image) { //얼굴 인식
-    const headers = {
-        "Access-Control-Allow-Origin" : "*"
-    }
-    const img = {
-        image : image
-    }
-    axios.post(`${BASE_URL}/ai/recognize/face`, img, {headers: headers}).then(
-        (res) => {
-            sessionStorage.setItem("eventFaceAge", res.data);
-            Swal.fire({
-                icon: 'info',
-                html: `${sessionStorage.getItem("eventFaceAge")}세이시군요!<br>다른 사람들 결과도 보러갈까요??!🤗`,
-                position: 'center',
-                showDenyButton: true,
-                confirmButtonText: '보러갈래요!',
-                denyButtonText: `아니요`,
-                })
-            .then((result) => {
-                if (result.isConfirmed) {
-                    document.getElementById("eventShow").style.display = "block";
-                    const nickname = document.getElementById("writeNick").value.trim();
-                    if(nickname !== '') {
-                        console.log(nickname);
-                        const participant = {
-                            "nickname": nickname,
-                            "age": `${sessionStorage.getItem("eventFaceAge")}`
-                        }
-                        writeRecord(participant);
-                    }
-                } else if (result.isDenied) {
-                    window.location.href='/'
-                }
-            })
-        }
-    )
-}
-
-function writeRecord(participant) { //정보 저장 및 조회
-    const headers = {
-        "Access-Control-Allow-Origin" : "*"
-    }
-    axios.post(`${BASE_URL}/event/record`, participant, {headers: headers})
-        .then((result) => {
-            
-        })
 }
 
