4d43e052d8bfbd8782f9606593955eb0a1bd4453
seojiwon0702 <seojiwon335@gmail.com>
2022-04-07 10:45:45 +0900

    [BE-서지원][combine-100] feat: add SementicSearch.py
    Former-commit-id: 2e87c00953f9611c47949c87506b182af3eaadcc

--- backend/fastapi/kobert/SemanticSearch.py
+++ backend/fastapi/kobert/SemanticSearch.py
@@ -1,43 +1,41 @@
 from sentence_transformers import SentenceTransformer, util
 import numpy as np
 
-model_path = './output/training_sts'
+model_path = '/workspace/fastapi/KoSentenceBERT_SKTBERT/output/training_sts'
 
 embedder = SentenceTransformer(model_path)
 
 # Corpus with example sentences
-corpus = ['한 남자가 음식을 먹는다.',
-          '한 남자가 빵 한 조각을 먹는다.',
-          '그 여자가 아이를 돌본다.',
-          '한 남자가 말을 탄다.',
-          '한 여자가 바이올린을 연주한다.',
-          '두 남자가 수레를 숲 솦으로 밀었다.',
-          '한 남자가 담으로 싸인 땅에서 백마를 타고 있다.',
-          '원숭이 한 마리가 드럼을 연주한다.',
-          '치타 한 마리가 먹이 뒤에서 달리고 있다.']
+corpus = ['조회 페이지로 이동해줘.',
+          '내 계좌 조회해줘.',
+          '이체 페이지로 이동해줘.',
+          '이체해줘.'
+          '금융상품 추천해줘.',
+          '예금 추천해줘.',
+          '적금 추천해줘.',
+          '예금 페이지로 이동해줘.',
+          '적금 페이지로 이동해줘.',
+          '상품 추천해줘.',
+          '얼굴인식 해줘.']
+
+keyword = ['조회', '조회', '이체', '이체', '금융', '금융', '금융', '금융', '금융', '추천', '추천']
 
 corpus_embeddings = embedder.encode(corpus, convert_to_tensor=True)
 
-# Query sentences:
-queries = ['한 남자가 파스타를 먹는다.',
-           '고릴라 의상을 입은 누군가가 드럼을 연주하고 있다.',
-           '치타가 들판을 가로 질러 먹이를 쫓는다.']
 
-# Find the closest 5 sentences of the corpus for each query sentence based on cosine similarity
-top_k = 5
-for query in queries:
+def semanticSearch(query):
+    # Find the closest 5 sentences of the corpus for each query sentence based on cosine similarity
+    top_k = 1
+
     query_embedding = embedder.encode(query, convert_to_tensor=True)
     cos_scores = util.pytorch_cos_sim(query_embedding, corpus_embeddings)[0]
     cos_scores = cos_scores.cpu()
 
-    #We use np.argpartition, to only partially sort the top_k results
+    # We use np.argpartition, to only partially sort the top_k results
     top_results = np.argpartition(-cos_scores, range(top_k))[0:top_k]
 
-    print("\n\n======================\n\n")
-    print("Query:", query)
-    print("\nTop 5 most similar sentences in corpus:")
+    result = corpus[top_results[0]].strip()
 
-    for idx in top_results[0:top_k]:
-        print(corpus[idx].strip(), "(Score: %.4f)" % (cos_scores[idx]))
-
-
+    for i in range(0, 11):
+        if result == corpus[i]:
+            return keyword[i]
